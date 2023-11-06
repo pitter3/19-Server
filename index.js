ded162df037c23b78b1ecb7643928073263ca70f
Patrick Eitel <patrickeitel12@gmail.com>
2023-11-06 13:24:05 -0800

    Add alliance rogue gear

--- index.js
+++ index.js
@@ -78,3 +78,64 @@ app.get('/factions', (req, res) => {
 
   res.status(200).send(factionImages);
 });
+
+app.get('/alliancerogue', (req, res) => {
+  const allianceRogueGear = [
+    {
+      itemName: 'Tunic of Westfall',
+      optional: false,
+      note: `BiS in Cata if you don't plan to buy Gloomshroud Armor.`,
+      questName: 'The Defias Brotherhood',
+      itemId: 2041,
+      questId: 166,
+      requiredLevel: 14,
+    },
+    {
+      name: 'Seal of Wrynn',
+      optional: false,
+      note: `Useful for high stam sets in Cata.`,
+      questName: 'An Audience with the King',
+      itemId: 2933,
+      questId: 396,
+      requiredLevel: 16,
+    },
+    {
+      name: 'Minor Channeling Ring',
+      optional: true,
+      note: `Currently BiS for max crit, probably won't be BiS in Cata.`,
+      questName: `WANTED: Chok'sul`,
+      itemId: 1449,
+      questId: 256,
+      requiredLevel: 9,
+    },
+    {
+      name: 'Dusty Mining Gloves',
+      optional: true,
+      note: `Change to +5 Agility +5 Stamina in Cata, might be useful in Cata.`,
+      questName: `Collecting Memories`,
+      itemId: 2036,
+      questId: 168,
+      requiredLevel: 14,
+    },
+    {
+      name: 'Gutterblade',
+      optional: true,
+      note: `Higher base DPS than fang, but likely too fast to ever be considered BiS.`,
+      questName: `Reane's Cleansing`,
+      itemId: 17046,
+      questId: 1046,
+      requiredLevel: 18,
+    },
+    {
+      name: 'Blackened Defias Armor',
+      optional: false,
+      note: `It's worth getting the full Defias set before Cata.`,
+      questName: null,
+      itemId: 10399,
+      questId: null,
+      requiredLevel: null,
+    },
+  ];
+
+  res.status(200).send(allianceRogueGear);
+});
